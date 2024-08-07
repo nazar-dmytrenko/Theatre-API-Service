43f4e6169b71b685aa75c2edaa37ed4c8566e343
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-08-07 15:04:46 +0300

    Create theatre and user views. Add permissions for theatre views

--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'theatre',
     'user',
+    'drf_spectacular',
 ]
 
 AUTH_USER_MODEL = 'user.User'
